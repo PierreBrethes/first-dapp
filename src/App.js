f71e16e7de69c2dfa5803946aa4677e50f4f6d62
Taco <pi.brethes@gmail.com>
2022-06-23 11:06:34 +0200

    [dev_001] connect wallet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import contract from './contracts/fristContract.json';
+
+const contractAddress = "0x355638a4eCcb777794257f22f50c289d4189F245";
+const abi = contract.abi;
 
 function App() {
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  const checkWalletIsConnected = () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Connect your wallet");
+      return;
+    } else {
+      console.log("Wallet exists !");
+    }
+  }
+
+  const connectWalletHandler = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      alert("Please install Metamask!");
+    }
+
+    try {
+      const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+      console.log("Found an account! Address: ", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const mintNftHandler = () => { }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  const mintNftButton = () => {
+    return (
+      <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+        Mint NFT
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-app'>
+      <h1>TacoCoins</h1>
+      <div>
+        {connectWalletButton()}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
